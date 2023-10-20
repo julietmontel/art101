c8b4b8d69ac5135958914e2ce9efcb7c69812a06
julietmontel <146909701+julietmontel@users.noreply.github.com>
2023-10-19 17:12:55 -0700

    Edits of Homepage and Lab 2
    Cleaned up some things and added new links, etc. Also added to lab 3 & 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock, Paper, Scissors
+// Author: Juilet & Raquel
+// Date:10/23/23
 
 // Constants
+
+//User input
+//  Rock or Paper or Scissors
+//Computer chooses 
+//  Random
+//Countdown
+//  3 seconds
+//Determine winner/loser of round
+//  Win:
+//    Rock > Scissors
+//    Paper > Rock
+//    Scissors > Paper
+//  Lose:
+//    Rock != Rock
+//    Paper != Paper
+//    Scissors != Scissors
+//Add points
+//Best of 3 games
+//  end after 3 games
+//Output winner
+
 
 // Functions
 
