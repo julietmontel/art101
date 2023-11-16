091dae802cc03461e93b7e39f30a8e5c3e3eff43
julietmontel <146909701+julietmontel@users.noreply.github.com>
2023-11-16 15:17:53 -0800

    lab 9 fixes
    fixed the jQuery version

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,3 +3,21 @@
 // Date: 11/10/2023
 
 
+//buttons
+$("#challenge").append("<button id='button-challenge'>Click me!</button>");
+$("#button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
+
+$("#problem").append("<button id='button-problem'>NO, click me!</button>")
+$("#button-problem").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problem").toggleClass("special-two");
+});
+
+$("#result").append("<button id='button-result'>Click me!!!</button>")
+$("#button-result").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#result").toggleClass("special-three");
+});
