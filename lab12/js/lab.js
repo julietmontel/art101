c9312dd1012b7e74b9c6539cc89e1f6047f8c139
julietmontel <146909701+julietmontel@users.noreply.github.com>
2023-11-24 20:21:30 -0800

    Lab 12 Edits
    same ol same ol, uses conditionals

---
+++ lab12/js/lab.js
@@ -0,0 +1,33 @@
+// index.js - Lab 12 - Condtionals
+// Author: Juliet Montelongo
+// Date: 11/24/23
+
+
+
+//function
+
+function sortingHat(str) {
+    len = str.length;
+    mod = len % 4;
+    if(mod == 0){
+        return "Gryffindor"
+    }
+    else if (mod == 1) {
+        return "Ravenclaw"
+    }
+    else if (mod == 2) {
+        return "Slytherin"
+    }
+    else if (mod == 3) {
+        return "Hufflepuff"
+    }
+}
+
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+    var name = document.getElementById("input").value
+    var house = sortingHat(name)
+    newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+    document.getElementById("output").innerHTML = newText;
+})
