71100066fc37b1b70c602112f0bdfc99700e64b2
julietmontel <146909701+julietmontel@users.noreply.github.com>
2023-12-07 22:46:47 -0800

    rest of lab 15 edits

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -2,3 +2,24 @@
 // Author: Juliet Montelongo
 // Date: 12/4/23
 
+
+
+$("#activate").click(function(){
+    $.ajax({
+        url: "https://yesno.wtf/api",
+        data: {
+            id: 123,
+            api_key: "blahblahblah",
+         },
+        type: "GET",
+        dataType : "json",
+        success: function(data){
+            $("#output").append('<div class="text"><p>' + " " + data.answer + '</p></div>');
+            console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("Error", textStatus, errorThrown);
+        }
+    });
+    
+ });
